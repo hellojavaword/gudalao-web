17b1b658fdec5588749aa2560a3aee4ddf384718
hellojavaword <43082970+hellojavaword@users.noreply.github.com>
2022-05-30 22:33:06 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '2ebe844673c64d258c391ebdbb1ca96e',
+  rootNotionPageId: '423d2e0b9d834a6ebe4e0bfd88efed90?v=2ebe844673c64d258c391ebdbb1ca96e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
